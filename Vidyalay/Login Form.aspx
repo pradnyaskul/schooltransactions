963bc90bcd26761b33039d25e46e31315498799f
pradnyaskul <pradnyaskul@yahoo.in>
2017-03-09 14:04:12 +0530

    Add files via upload

---
+++ Vidyalay/Login Form.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login Form.aspx.cs" Inherits="Login_Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 40%;
+            margin-top: 104px;
+        }
+        .auto-style2 {
+            width: 109px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table align="center" class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label1" runat="server" Text="First_Name"></asp:Label>
+                </td>
+                <td>
+                    <asp:RadioButton ID="RadioButton1" runat="server" style="z-index: 1; left: 445px; top: 124px " AutoPostBack="True" GroupName="l" OnCheckedChanged="RadioButton1_CheckedChanged" Text="English" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label2" runat="server" Text="Last_Name"></asp:Label>
+                </td>
+                <td>
+                    <asp:RadioButton ID="RadioButton2" runat="server" style="z-index: 1; left: 451px; top: 144px " AutoPostBack="True" Checked="True" GroupName="l" OnCheckedChanged="RadioButton2_CheckedChanged" Text="Marathi" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    &nbsp;<asp:Label ID="Label3" runat="server" Text="Address"></asp:Label>
+                </td>
+                <td>
+                    <asp:RadioButton ID="RadioButton3" runat="server" style="z-index: 1; left: 443px; top: 175px " AutoPostBack="True" GroupName="l" OnCheckedChanged="RadioButton3_CheckedChanged" Text="Kannada" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="z-index: 1; left: 496px; top: 191px ; right: 539px" Text="English" />
+                    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" style="z-index: 1; left: 392px; top: 191px " Text="Marathi" />
+                    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click"   Text="Kannada" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Vidyalay/Login Form.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+using System.Reflection;
+
+
+public partial class Login_Form : System.Web.UI.Page
+{
+    ResourceManager rm;
+    CultureInfo ci;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("mr-IN");
+            rm = new ResourceManager("Resources.Strings", System.Reflection.Assembly.Load("App_GlobalResources"));
+            ci = Thread.CurrentThread.CurrentCulture;
+            LoadString(ci);
+        }
+        else
+        {
+            rm = new ResourceManager("Resources.Strings", System.Reflection.Assembly.Load("App_GlobalResources"));    
+            ci = Thread.CurrentThread.CurrentCulture;
+            LoadString(ci);
+        }
+    }
+        private void LoadString(CultureInfo ci)
+    {
+
+        Label1.Text = rm.GetString("First_Name",ci);
+        Label2.Text = rm.GetString("Last_Name",ci);
+
+        Label3.Text = rm.GetString("Address",ci);
+       
+    }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            LoadString(Thread.CurrentThread.CurrentCulture);
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("mr-IN");
+            LoadString(Thread.CurrentThread.CurrentCulture);
+
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("kn-IN");
+            LoadString(Thread.CurrentThread.CurrentCulture);
+
+        }
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                LoadString(Thread.CurrentThread.CurrentCulture);
+            }
+        }
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("mr-IN");
+            LoadString(Thread.CurrentThread.CurrentCulture);
+
+        }
+        protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("kn-IN");
+            LoadString(Thread.CurrentThread.CurrentCulture);
+
+        }
+}
